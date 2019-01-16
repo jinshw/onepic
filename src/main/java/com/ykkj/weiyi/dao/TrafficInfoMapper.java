514673707585fa83fdc38c681eb890ba6b833e20
jinshw <jinshengwang2007@163.com>
2019-01-16 09:23:43 +0800

    解决拥堵查询报错

--- src/main/java/com/ykkj/weiyi/dao/TrafficInfoMapper.java
+++ src/main/java/com/ykkj/weiyi/dao/TrafficInfoMapper.java
@@ -1,11 +1,13 @@
 package com.ykkj.weiyi.dao;
 
+import com.ykkj.weiyi.datasource.DataSource;
 import com.ykkj.weiyi.pojo.DelInfo;
 import com.ykkj.weiyi.pojo.DelInfoExample;
 import com.ykkj.weiyi.pojo.TrafficInfo;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+
 
 public interface TrafficInfoMapper {
     public List<TrafficInfo> findList(@Param("pojo") TrafficInfo trafficInfo);
--- src/main/java/com/ykkj/weiyi/service/impl/TrafficInfoServiceImpl.java
+++ src/main/java/com/ykkj/weiyi/service/impl/TrafficInfoServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@DataSource("dataSourceMys")
 public class TrafficInfoServiceImpl implements TrafficInfoService {
     
     @Autowired
